fad9cf31e22d5048f6a877dba9367816e13918a5
crimme <kueppers.fabian@web.de>
2024-11-05 11:23:35 +0100

    drawed character and enemies

--- models/fish.class.js
+++ models/fish.class.js
@@ -1,1 +1,5 @@
-class Fish extends MovableObject {}
+class Fish extends MovableObject {
+  constructor() {
+    super().loadImage("../img/2.Enemy/Pufferfish/1.Swim/1.swim1.png");
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,39 @@
 class World {
   character = new Character();
   enemies = [new Fish(), new Fish(), new Fish(), new Fish()];
+  canvas;
+  ctx;
 
-  draw() {}
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.height,
+      this.character.width
+    );
+
+    for (let index = 0; index < this.enemies.length; index++) {
+      const element = this.enemies[index];
+      this.ctx.drawImage(
+        element.img,
+        element.x,
+        element.y,
+        element.height,
+        element.width
+      );
+    }
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }

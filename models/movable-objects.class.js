ac65bfe21f7c4df770353bf9c0635331cc1cc430
crimme <kueppers.fabian@web.de>
2024-11-05 10:26:07 +0100

    classes added

---
+++ models/movable-objects.class.js
@@ -0,0 +1,20 @@
+class MovableObject {
+  x = 120;
+  y = 400;
+  img;
+
+  moveRight() {
+    console.log("moving right");
+  }
+  moveLeft() {
+    console.log("moving left");
+  }
+
+  moveUp() {
+    console.log("moving up");
+  }
+
+  moveDown() {
+    console.log("moving down");
+  }
+}

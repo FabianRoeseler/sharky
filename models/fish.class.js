e00570be9c231bf94b478f17d20b8c192e096afe
crimme <kueppers.fabian@web.de>
2024-11-07 10:05:39 +0100

    fixed axes

--- models/fish.class.js
+++ models/fish.class.js
@@ -2,7 +2,7 @@ class Fish extends MovableObject {
   constructor() {
     super().loadImage("../img/2.Enemy/Pufferfish/1.Swim/1.swim1.png");
     this.x = 190 + Math.random() * 700 + 300;
-    this.y = Math.random() * 400;
+    this.y = Math.random() * 300;
     this.height = 110;
     this.width = 75;
   }
